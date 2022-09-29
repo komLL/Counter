dc56f7107f03a1e463f7819211a7c80a0938ad3b
github komLL <git_komalupa.lee1996@gmail.com>
2022-09-29 10:50:57 +0530

    counter

--- src/Cycle.js
+++ src/Cycle.js
@@ -6,11 +6,13 @@ const Cycle=()=>{
     const[count,setCount]=useState(0);
     const increm=()=>{
         setCount(count+1)
+        //increment
     }
     const decrem=()=>{
         if(count>0){setCount(count-1)}
         else{setCount(0)
         alert("value cannot be decrease beyond this limit")}
+        //decrement
     }
     
     return(
